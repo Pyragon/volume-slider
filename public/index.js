a8a3c91aeb854ec41b7a8decee4b85242195094b
Cody Thompson <eldo.imo.rs@hotmail.com>
2017-06-04 01:53:57 -0700

    Add moving meme slider

--- public/index.js
+++ public/index.js
@@ -1,3 +1,72 @@
 $(document).ready(function() {
-  
+
+  var canvas = $('<canvas/>').attr({width:150, height:300}).appendTo('body');
+
+  var context = canvas.get(0).getContext('2d');
+
+  var block_size = 15; //10 blocks
+  var h = (300/block_size);
+  var w = (150/block_size);
+
+  var cur_y = h-1;
+
+  var cur_x = 0;
+  var our_block_size = 6;
+
+  var direction = 'right';
+
+  var fps = 2;
+
+  var loss = false;
+
+  tick();
+
+  function start() {
+    fillBlocks('red');
+    setTimeout(tick, 1000 / fps);
+  }
+
+  function tick() {
+    if(loss == true) return;
+    setTimeout(tick, 1000 / fps);
+    eraseBlocks();
+    move(direction);
+  }
+
+  function fillBlocks(colour) {
+    for(var i = 0; i < our_block_size; i++) {
+      var index = cur_x + i;
+      context.fillStyle = colour;
+      context.fillRect(index * block_size, cur_y * block_size, block_size, block_size);
+    }
+  }
+
+  function eraseBlocks() {
+    for(var i = 0; i < w; i++) {
+      context.fillStyle = 'white';
+      context.fillRect(i * block_size, cur_y * block_size, block_size, block_size);
+    }
+  }
+
+  function pastEnd(x) {
+    if(direction === 'right') return x+our_block_size > w;
+    else if(direction === 'left') return x < 0;
+  }
+
+  function move(direction) {
+    cur_x += (direction === 'right' ? 1 : -1);
+    fillBlocks('red');
+    if(pastEnd(cur_x + ((direction === 'right' ? 1 : -1))))
+      changeDirection();
+  }
+
+  function changeDirection() {
+    if(direction === 'left') direction = 'right';
+    else if(direction === 'right') direction = 'left';
+  }
+
+  function topUp() {
+
+  }
+
 });
